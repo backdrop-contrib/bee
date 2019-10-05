50ff775efebb92fd62a9816955f5c2d6319683d3
PackWeb <info@packweb.eu>
2019-10-05 22:30:57 +0300

    Issue #19: Started adding core tests.

--- tests/CoreTest.php
+++ tests/CoreTest.php
@@ -9,11 +9,46 @@ use PHPUnit\Framework\TestCase;
 class CoreTest extends TestCase {
 
   /**
-   * Make sure running `b` works.
+   * Make sure that `b` is installed correctly.
    */
   public function testBInstalledCorrectly() {
-    exec('b', $output);
-    $this->assertContains('Backdrop Console commands:', $output);
+    exec('b', $output, $result);
+    $this->assertEquals(0, $result);
+  }
+
+  /**
+   * Make sure that `help` is used as the the default command (when no command
+   * is given).
+   */
+  public function testHelpIsDefaultCommand() {
+    exec('b', $output_b);
+    exec('b help', $output_help);
+    $this->assertEquals($output_help, $output_b);
+  }
+
+  /**
+   * Make sure that required arguments are actually required.
+   */
+  public function testRequiredArgumentsAreRequired() {
+    exec('b config-get', $output);
+    $this->assertStringContainsString('Argument config_name is required', $output);
+  }
+
+  /**
+   * Make sure that command aliases work.
+   */
+  public function testCommandAliasesWork() {
+    exec('b st', $output_alias);
+    exec('b core-status', $output_command);
+    $this->assertEquals($output_command, $output_alias);
+  }
+
+  /**
+   * Make sure that the `--y/--yes` options work.
+   */
+  public function testYesOptionWorks() {
+    exec('b en book', $output);
+    $this->assertStringContainsString('Module book enabled', $output);
   }
 
 }
