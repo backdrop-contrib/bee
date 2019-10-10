c92c077e1de3631b0aa7583287433d0a03bed78b
Peter Anderson <peter@panda.id.au>
2019-10-10 21:39:05 +0300

    Added new tests, cleaned up old ones.

--- tests/CommandsCoreTest.php
+++ tests/CommandsCoreTest.php
@@ -13,11 +13,12 @@ class CommandsCoreTest extends TestCase {
    */
   public function testCacheClear() {
     $output_all = shell_exec('b cc all');
-    $output_menu = shell_exec('b cc menu');
-    $output_update = shell_exec('b cc update');
-
     $this->assertStringContainsString('All cache cleared', $output_all);
+
+    $output_menu = shell_exec('b cc menu');
     $this->assertStringContainsString('Menu cache cleared', $output_menu);
+
+    $output_update = shell_exec('b cc update');
     $this->assertStringContainsString('Update data cache cleared', $output_update);
   }
 
@@ -26,7 +27,6 @@ class CommandsCoreTest extends TestCase {
    */
   public function testConfigList() {
     $output = shell_exec('b config-list');
-
     $this->assertStringContainsString('system.core', $output);
     $this->assertStringContainsString('System core settings', $output);
   }
@@ -36,7 +36,6 @@ class CommandsCoreTest extends TestCase {
    */
   public function testConfigGet() {
     $output = shell_exec('b config-get system.core');
-
     $this->assertStringContainsString('site_favicon_path', $output);
     $this->assertStringContainsString('core/misc/favicon.ico', $output);
   }
@@ -48,25 +47,27 @@ class CommandsCoreTest extends TestCase {
     $config = 'system.core';
     $setting = 'site_frontpage';
 
-    exec("b config-get $config", $output_get_old);
-    $output_set = shell_exec("b config-set $config $setting about");
-    exec("b config-get $config", $output_get_new);
-
     // Make sure the old homepage is 'home'.
+    exec("b config-get $config", $output_get_old);
     foreach ($output_get_old as $output) {
       if (strpos($output, $setting) !== FALSE) {
         $this->assertStringContainsString('home', $output);
       }
     }
 
+    $output_set = shell_exec("b config-set $config $setting about");
     $this->assertStringContainsString('Config updated', $output_set);
 
     // Make sure the new homepage is 'about'.
+    exec("b config-get $config", $output_get_new);
     foreach ($output_get_new as $output) {
       if (strpos($output, $setting) !== FALSE) {
         $this->assertStringContainsString('about', $output);
       }
     }
+
+    // Set frontpage back to 'home' for future tests.
+    exec("b config-set $config $setting home");
   }
 
   /**
@@ -74,8 +75,51 @@ class CommandsCoreTest extends TestCase {
    */
   public function testCron() {
     $output = shell_exec('b cron');
-
     $this->assertStringContainsString('Cron processed', $output);
+  }
+
+  /**
+   * Check that the Watchdog Show command works.
+   */
+  public function testWatchdogShow() {
+    $output_all = shell_exec('b ws');
+    $this->assertStringContainsString('Date', $output_all);
+    $this->assertStringContainsString('Message', $output_all);
+
+    $output_one = shell_exec('b ws 1');
+    $this->assertStringContainsString('ID', $output_one);
+    $this->assertStringContainsString('Severity', $output_one);
+
+    exec('b ws --count=2', $output_count);
+    $this->assertEquals(3, count($output_count));
+
+    exec('b ws --severity=info', $output_severity);
+    $this->assertStringNotContainsString('notice', $output_severity);
+
+    exec('b ws --type=cron', $output_type);
+    $this->assertStringNotContainsString('system', $output_type);
+  }
+
+  /**
+   * Check that the Site Install command works.
+   */
+  public function testSiteInstall() {
+    // Since this test uses environment-specific information, it is only run on
+    // Travis CI.
+    if (getenv('TRAVIS') !== TRUE) {
+      $this->markTestSkipped('This test is only run on Travis CI.');
+    }
+
+    $path = getenv('HOME') . '/site_install';
+    $db = 'site_install';
+
+    // Download Backdrop and create database.
+    exec("git clone https://github.com/backdrop/backdrop.git $path");
+    exec("mysql -e 'CREATE DATABASE $db'");
+
+    $output = shell_exec("b si --root=$path --db_url=mysql://travis:@127.0.0.1/$db");
+    print_r($output);
+    //$this->assertStringContainsString('Cron processed', $output);
   }
 
 }
--- tests/CoreTest.php
+++ tests/CoreTest.php
@@ -12,10 +12,8 @@ class CoreTest extends TestCase {
    * Make sure that `b` is installed correctly.
    */
   public function testBInstalledCorrectly() {
-    // Capture the exit status of the command.
-    exec('b', $output, $result);
-
     // A '0' exit status means success.
+    exec('b', $output, $result);
     $this->assertEquals(0, $result);
   }
 
@@ -26,7 +24,6 @@ class CoreTest extends TestCase {
     // Need to change this if we ever add a 'spoon' command ;-)
     $command = 'spoon';
     $output = shell_exec("b $command");
-
     $this->assertStringContainsString("There is no $command", $output);
   }
 
@@ -36,7 +33,6 @@ class CoreTest extends TestCase {
   public function testRequiredArgumentsAreRequired() {
     // `config-get` has a required argument: 'config_name'.
     $output = shell_exec('b config-get');
-
     $this->assertStringContainsString('Argument config_name is required', $output);
   }
 
@@ -47,22 +43,23 @@ class CoreTest extends TestCase {
     // `st` is an alias for `core-status`.
     $output_command = shell_exec('b core-status');
     $output_alias = shell_exec('b st');
-
     $this->assertEquals($output_command, $output_alias);
   }
 
   /**
    * Make sure that the `--root` option works.
+   *
+   * Test running `b` from inside Backdrop, outside Backdrop, and then with the
+   * '--root' option.
    */
   public function testRootOptionWorks() {
-    // Test running `b` from inside Backdrop, outside Backdrop, and then with
-    // the '--root' option.
     $output_backdrop = shell_exec('b st');
-    $output_not_backdrop = shell_exec('cd ../ && b st');
-    $output_root = shell_exec('cd ../ && b --root=www st');
-
     $this->assertStringContainsString('Backdrop CMS Installation detected', $output_backdrop);
+
+    $output_not_backdrop = shell_exec('cd ../ && b st');
     $this->assertStringContainsString('No Backdrop installation found', $output_not_backdrop);
+
+    $output_root = shell_exec('cd ../ && b --root=www st');
     $this->assertEquals($output_backdrop, $output_root);
   }
 
@@ -71,10 +68,11 @@ class CoreTest extends TestCase {
    */
   public function testYesOptionWorks() {
     $module = 'book';
-    $output_y = shell_exec("b en $module --y");
-    $output_yes = shell_exec("b dis $module --yes");
 
+    $output_y = shell_exec("b en $module --y");
     $this->assertStringContainsString("Module $module enabled", $output_y);
+
+    $output_yes = shell_exec("b dis $module --yes");
     $this->assertStringContainsString("Module $module disabled", $output_yes);
   }
 
