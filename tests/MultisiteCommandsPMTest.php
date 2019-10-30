9d2e258040ee550fea9398254957a47f2b20b6d1
Peter Anderson <peter@panda.id.au>
2019-10-30 14:26:30 +0200

    Added multisite  test.

---
+++ tests/MultisiteCommandsPMTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file
+ * PHPUnit tests for Backdrop Console multisite PM commands.
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class MultisiteCommandsPMTest extends TestCase {
+
+  /**
+   * Check that the PM Download command works for multisites.
+   */
+  public function testPmDownloadMultisite() {
+    // Root directory.
+    $output_root = shell_exec('b dl simplify');
+    $this->assertStringContainsString('simplify downloaded to ' . BACKDROP_ROOT . '/modules/simplify', $output_root);
+    $this->assertTrue(file_exists('modules/simplify/simplify.info'));
+
+    // Site directory.
+    $output_site = shell_exec('cd sites/multisite && b dl borg');
+    $this->assertStringContainsString('borg downloaded to ' . BACKDROP_ROOT . '/sites/multisite/themes/borg', $output_site);
+    $this->assertTrue(file_exists('sites/multisite/themes/borg/borg.info'));
+
+    // Remove downloads for future tests.
+    exec('rm -r modules/simplify sites/multisite/themes/borg');
+  }
+
+}
