488de925c8c10869005f86fc0c551bfd4b88b321
Peter Anderson <peter@panda.id.au>
2019-10-30 15:53:11 +0200

    Fixed root dir.

--- tests/MultisiteCommandsPMTest.php
+++ tests/MultisiteCommandsPMTest.php
@@ -12,14 +12,16 @@ class MultisiteCommandsPMTest extends TestCase {
    * Check that the PM Download command works for multisites.
    */
   public function testPmDownloadMultisite() {
+    $root = shell_exec('pwd');
+
     // Root directory.
     $output_root = shell_exec('b dl simplify');
-    $this->assertStringContainsString('simplify downloaded to ' . $HOME . '/www/modules/simplify', $output_root);
+    $this->assertStringContainsString('simplify downloaded to ' . $root . '/modules/simplify', $output_root);
     $this->assertTrue(file_exists('modules/simplify/simplify.info'));
 
     // Site directory.
     $output_site = shell_exec('cd sites/multisite && b dl borg');
-    $this->assertStringContainsString('borg downloaded to ' . $HOME . '/www/sites/multisite/themes/borg', $output_site);
+    $this->assertStringContainsString('borg downloaded to ' . $root . '/sites/multisite/themes/borg', $output_site);
     $this->assertTrue(file_exists('sites/multisite/themes/borg/borg.info'));
 
     // Remove downloads for future tests.
