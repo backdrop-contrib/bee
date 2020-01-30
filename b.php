3698008217f281f0f9eeb0da0855c6b9962be4f4
Peter Anderson <peter@panda.id.au>
2020-01-30 22:49:56 +0200

    saving work.

--- b.php
+++ b.php
@@ -6,50 +6,25 @@
  */
 
 // Set custom error handler.
-set_error_handler('b_errorHandler');
+set_error_handler('b_error_handler');
 
 // Include files.
+require_once __DIR__ . '/includes/globals.inc';
 require_once __DIR__ . '/includes/common.inc';
 require_once __DIR__ . '/includes/command.inc';
 require_once __DIR__ . '/includes/input_output.inc';
 require_once __DIR__ . '/includes/render.inc';
 require_once __DIR__ . '/includes/filesystem.inc';
 
-// Global variables.
-$global_options = array(
-  'root' => array(
-    'description' => bt('Specify the root directory of the Backdrop installation to use. If not set, will try to find the Backdrop installation automatically based on the current directory.'),
-  ),
-  'url' => array(
-    'description' => bt('Specify the URL of the Backdrop site to use (as defined in `sites.php`). If not set, will try to find the Backdrop site automatically based on the current directory.'),
-  ),
-  'drush' => array(
-    'description' => bt('Use `.drush.inc` files instead. Drupal 7 Drush commands compatibility.'),
-  ),
-  'yes' => array(
-    'short' => 'y',
-    'description' => bt('Answer `yes` automatically to all questions without prompting.'),
-  ),
-  'debug' => array(
-    'short' => 'd',
-    'description' => bt('Enables `debug` mode.'),
-  ),
-);
-$command = NULL;
-$options = array();
-$arguments = array();
-$multisites = array();
-$elements = array();
-
 // Main execution code.
 b_init();
 b_command_process();
 b_print_messages();
-b_render($elements);
+b_render();
 exit();
 
 /**
- * Setup a custom error handler.
+ * Custom error handler for `b`.
  *
  * @param int $errno
  *   The level of the error.
@@ -62,7 +37,7 @@ exit();
  * @param array $context
  *   An array of all variables from where the error was triggered.
  */
-function b_errorHandler($errno, $message, $filename, $line, $context) {
+function b_error_handler($errno, $message, $filename, $line, $context) {
   if (error_reporting() > 0) {
     echo "$message\n";
     echo " $filename:$line\n\n";
