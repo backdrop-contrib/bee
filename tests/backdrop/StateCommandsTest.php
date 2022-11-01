2e1bb93be22fd8f8f8132a3d96c69d984cc3f9aa
Martin Price <github@systemhorizons.co.uk>
2022-11-01 14:42:51 +0000

    Switch to return values and validation Added validation to setting states and switched to return values.

--- tests/backdrop/StateCommandsTest.php
+++ tests/backdrop/StateCommandsTest.php
@@ -12,28 +12,30 @@ class StateCommandsTest extends TestCase {
    * Make sure that the state-get command works.
    */
   public function test_state_get_command_works() {
-    // New installs don't have maintenance_mode state so need setting
-    $output = shell_exec('bee maintenance_mode FALSE');
+    // New installs don't have maintenance_mode state so need setting to TRUE to initiate
+    $output = shell_exec('bee maintenance-mode TRUE');
+    $output = shell_exec('bee maintenance-mode FALSE');
     
+    // Make sure maintenance mode is disabled and message matches format
     $output = shell_exec('bee state-get maintenance_mode');
-    $this->assertStringContainsString("The value of the 'maintenance_mode' state is: FALSE", (string) $output);
+    $this->assertStringContainsString("The value (boolean) of the 'maintenance_mode' state is: FALSE", (string) $output);
   }
 
   /**
    * Make sure that the state-set command works.
    */
   public function test_state_set_command_works() {
-    // Make sure maintenance_mode is '0'.
+    // Make sure maintenance_mode is disabled.
     $output = shell_exec('bee state-get maintenance_mode');
-    $this->assertStringContainsString("The value of the 'maintenance_mode' state is: FALSE", (string) $output);
+    $this->assertStringContainsString("The value (boolean) of the 'maintenance_mode' state is: FALSE", (string) $output);
     
-    // Set maintenance_mode to '1'.
+    // Set maintenance_mode to 'TRUE'.
     $output = shell_exec('bee state-set maintenance_mode TRUE');
     $this->assertStringContainsString("The 'maintenance_mode' state was set to: TRUE", (string) $output);
     
-    // Make sure maintenance_mode is '1'.
+    // Make sure maintenance_mode is enabled.
     $output = shell_exec('bee state-get maintenance_mode');
-    $this->assertStringContainsString("The value of the 'maintenance_mode' state is: TRUE", (string) $output);
+    $this->assertStringContainsString("The value (boolean) of the 'maintenance_mode' state is: TRUE", (string) $output);
   }
 
   /**
