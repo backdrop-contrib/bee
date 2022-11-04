288cc294c611881c3da9bd29117580fe8b205f8c
Martin Price <github@systemhorizons.co.uk>
2022-11-04 09:12:39 +0000

    Action feedback and update tests

--- tests/backdrop/StateCommandsTest.php
+++ tests/backdrop/StateCommandsTest.php
@@ -12,13 +12,14 @@ class StateCommandsTest extends TestCase {
    * Make sure that the state-get command works.
    */
   public function test_state_get_command_works() {
-    // New installs don't have maintenance_mode state so need setting to TRUE to initiate
+    // New installs don't have maintenance_mode state set. Set it to TRUE to
+    // initiate it.
     $output = shell_exec('bee maintenance-mode TRUE');
     $output = shell_exec('bee maintenance-mode FALSE');
 
-    // Make sure maintenance mode is disabled and message matches format
+    // Make sure maintenance mode is disabled and message matches format.
     $output = shell_exec('bee state-get maintenance_mode');
-    $this->assertStringContainsString("The value (boolean) of the 'maintenance_mode' state is: FALSE", (string) $output);
+    $this->assertStringContainsString("The value of the 'maintenance_mode' state is: FALSE (boolean)", (string) $output);
   }
 
   /**
@@ -27,7 +28,7 @@ class StateCommandsTest extends TestCase {
   public function test_state_set_command_works() {
     // Make sure maintenance_mode is disabled.
     $output = shell_exec('bee state-get maintenance_mode');
-    $this->assertStringContainsString("The value (boolean) of the 'maintenance_mode' state is: FALSE", (string) $output);
+    $this->assertStringContainsString("The value of the 'maintenance_mode' state is: FALSE (boolean)", (string) $output);
 
     // Set maintenance_mode to 'TRUE'.
     $output = shell_exec('bee state-set maintenance_mode TRUE');
@@ -35,34 +36,34 @@ class StateCommandsTest extends TestCase {
 
     // Make sure maintenance_mode is enabled.
     $output = shell_exec('bee state-get maintenance_mode');
-    $this->assertStringContainsString("The value (boolean) of the 'maintenance_mode' state is: TRUE", (string) $output);
+    $this->assertStringContainsString("The value of the 'maintenance_mode' state is: TRUE (boolean)", (string) $output);
   }
 
   /**
    * Make sure that the maintenance-mode command works.
    */
   public function test_maintenance_mode_command_works() {
-    // Attempt to enable maintenance mode
+    // Attempt to enable maintenance mode.
     $output = shell_exec('bee maintenance-mode TRUE');
     $this->assertStringContainsString('Maintenance mode is already enabled', (string) $output);
 
-    // Disable maintenance mode
+    // Disable maintenance mode.
     $output = shell_exec('bee maintenance-mode FALSE');
     $this->assertStringContainsString('Maintenance mode was disabled', (string) $output);
 
-    // Enable maintenance mode
+    // Enable maintenance mode.
     $output = shell_exec('bee maintenance-mode TRUE');
     $this->assertStringContainsString('Maintenance mode was enabled', (string) $output);
 
-    // Get the status of maintenance mode
+    // Get the status of maintenance mode.
     $output = shell_exec('bee maintenance-mode');
     $this->assertStringContainsString('Maintenance mode is enabled', (string) $output);
 
-    // Disable maintenance mode
+    // Disable maintenance mode.
     $output = shell_exec('bee maintenance-mode FALSE');
     $this->assertStringContainsString('Maintenance mode was disabled', (string) $output);
 
-    // Get the status of maintenance mode
+    // Get the status of maintenance mode.
     $output = shell_exec('bee maintenance-mode');
     $this->assertStringContainsString('Maintenance mode is disabled', (string) $output);
   }
