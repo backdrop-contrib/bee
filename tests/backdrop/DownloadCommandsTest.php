ddd925aed0bc7078c041866b8a173a1243d6c9a7
indigoxela <xela@indigofloat.at>
2023-07-14 10:03:38 +0200

    Issue #289: Replace exec() and shell_exec() in download command (#310)
    * Issue #289: Replace exec() and shell_exec() in download command with php curl
    * Define a constant for useragent string
    * Clean up tests, remove usage of hide-progress option

--- tests/backdrop/DownloadCommandsTest.php
+++ tests/backdrop/DownloadCommandsTest.php
@@ -15,12 +15,12 @@ class DownloadCommandsTest extends TestCase {
    */
   public function test_download_command_works() {
     // Single module.
-    $output_single = shell_exec('bee download --hide-progress simplify');
+    $output_single = shell_exec('bee download simplify');
     $this->assertStringContainsString("'simplify' was downloaded into '/app/backdrop/modules/simplify'.", (string) $output_single);
     $this->assertTrue(file_exists('/app/backdrop/modules/simplify/simplify.info'));
 
     // Multiple projects (theme and layout).
-    $output_multiple = shell_exec('bee download --hide-progress lumi bamboo');
+    $output_multiple = shell_exec('bee download lumi bamboo');
     $this->assertStringContainsString("'lumi' was downloaded into '/app/backdrop/themes/lumi'.", (string) $output_multiple);
     $this->assertTrue(file_exists('/app/backdrop/themes/lumi/lumi.info'));
     $this->assertStringContainsString("'bamboo' was downloaded into '/app/backdrop/layouts/bamboo'.", (string) $output_multiple);
@@ -35,12 +35,12 @@ class DownloadCommandsTest extends TestCase {
    */
   public function test_download_core_command_works() {
     // Download to current directory.
-    $output_current = shell_exec('mkdir /app/current && cd /app/current && bee download-core --hide-progress');
+    $output_current = shell_exec('mkdir /app/current && cd /app/current && bee download-core');
     $this->assertStringContainsString("Backdrop was downloaded into '/app/current'.", (string) $output_current);
     $this->assertTrue(file_exists('/app/current/index.php'));
 
     // Download to specified directory.
-    $output_directory = shell_exec('bee download-core --hide-progress /app/directory');
+    $output_directory = shell_exec('bee download-core /app/directory');
     $this->assertStringContainsString("Backdrop was downloaded into '/app/directory'.", (string) $output_directory);
     $this->assertTrue(file_exists('/app/directory/index.php'));
 
