294c50c4049bda66a325cc88b251316a9f14681d
Martin Price <github@systemhorizons.co.uk>
2024-08-30 19:47:08 +0100

    Add theme debug command

--- tests/backdrop/ThemeCommandsTest.php
+++ tests/backdrop/ThemeCommandsTest.php
@@ -33,4 +33,28 @@ class ThemeCommandsTest extends TestCase {
     exec('bee theme-admin seven');
   }
 
+  /**
+   * Make sure that the theme-debug command works.
+   */
+  public function test_theme_debug_command_works() {
+    // Attempt to disable theme debug.
+    $output = shell_exec('bee theme-debug FALSE');
+    $this->assertStringContainsString('Theme debug is already disabled', (string) $output);
+
+    // Enable theme debug.
+    $output = shell_exec('bee theme-debug TRUE');
+    $this->assertStringContainsString('Theme debug was enabled', (string) $output);
+
+    // Get the status of theme debug.
+    $output = shell_exec('bee theme-debug');
+    $this->assertStringContainsString('Theme debug is enabled', (string) $output);
+
+    // Disable theme debug.
+    $output = shell_exec('bee theme-debug FALSE');
+    $this->assertStringContainsString('Theme debug was disabled', (string) $output);
+
+    // Get the status of theme debug.
+    $output = shell_exec('bee theme-debug');
+    $this->assertStringContainsString('Theme debug is disabled', (string) $output);
+  }
 }
