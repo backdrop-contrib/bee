9b2b2e7d09bf4ff1f639020c771306f20b5bb1e8
Martin Price <github@systemhorizons.co.uk>
2025-04-17 18:39:08 +0100

    Allow cancel from select menu. Handle negative choices. Adjust tests.

--- tests/backdrop/DownloadCommandsTest.php
+++ tests/backdrop/DownloadCommandsTest.php
@@ -17,14 +17,17 @@ class DownloadCommandsTest extends TestCase {
     global $bee_test_root;
     // Single module.
     $output_single = shell_exec('bee download simplify');
-    $this->assertStringContainsString("'simplify' was downloaded into '$bee_test_root/backdrop/modules/simplify'.", (string) $output_single);
+    $pattern = '/\'simplify\' \([\w\s\.\W]*\) was downloaded into \'' . preg_quote($bee_test_root, '/') . '\/backdrop\/modules\/simplify\'/';
+    $this->assertRegExp($pattern, $output_single);
     $this->assertTrue(file_exists("$bee_test_root/backdrop/modules/simplify/simplify.info"));
 
     // Multiple projects (theme and layout).
     $output_multiple = shell_exec('bee download lumi bamboo');
-    $this->assertStringContainsString("'lumi' was downloaded into '$bee_test_root/backdrop/themes/lumi'.", (string) $output_multiple);
+    $pattern = '/\'lumi\' \([\w\s\.\W]*\) was downloaded into \'' . preg_quote($bee_test_root, '/') . '\/backdrop\/themes\/lumi\'/';
+    $this->assertRegExp($pattern, $output_multiple);
     $this->assertTrue(file_exists("$bee_test_root/backdrop/themes/lumi/lumi.info"));
-    $this->assertStringContainsString("'bamboo' was downloaded into '$bee_test_root/backdrop/layouts/bamboo'.", (string) $output_multiple);
+    $pattern = '/\'bamboo\' \([\w\s\.\W]*\) was downloaded into \'' . preg_quote($bee_test_root, '/') . '\/backdrop\/layouts\/bamboo\'/';
+    $this->assertRegExp($pattern, $output_multiple);
     $this->assertTrue(file_exists("$bee_test_root/backdrop/layouts/bamboo/bamboo.info"));
 
     // Cleanup downloads.
@@ -38,12 +41,14 @@ class DownloadCommandsTest extends TestCase {
     global $bee_test_root;
     // Download to current directory.
     $output_current = shell_exec("mkdir $bee_test_root/current && cd $bee_test_root/current && bee download-core");
-    $this->assertStringContainsString("Backdrop was downloaded into '$bee_test_root/current'.", (string) $output_current);
+    $pattern = '/Backdrop \([\w\s\.\W]*\) was downloaded into \'' . preg_quote($bee_test_root, '/') . '\/current\'/';
+    $this->assertRegExp($pattern, $output_current);
     $this->assertTrue(file_exists("$bee_test_root/current/index.php"));
 
     // Download to specified directory.
     $output_directory = shell_exec("bee download-core $bee_test_root/directory");
-    $this->assertStringContainsString("Backdrop was downloaded into '$bee_test_root/directory'.", (string) $output_directory);
+    $pattern = '/Backdrop \([\w\s\.\W]*\) was downloaded into \'' . preg_quote($bee_test_root, '/') . '\/directory\'/';
+    $this->assertRegExp($pattern, $output_directory);
     $this->assertTrue(file_exists("$bee_test_root/directory/index.php"));
 
     // Cleanup downloads.
